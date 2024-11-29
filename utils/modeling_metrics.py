45e9571e0e9c7c1449d2b5f82719f4153712486c
dan-teixeira00 <daniel.teixeira@infoprice.co>
2024-11-28 22:28:43 -0300

    Modeling progress + confusion matrix function

--- utils/modeling_metrics.py
+++ utils/modeling_metrics.py
@@ -3,9 +3,11 @@
 """
 
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 from statsmodels.stats.outliers_influence import variance_inflation_factor
 from statsmodels.api import add_constant
-
+from sklearn.metrics import accuracy_score, recall_score
 
 
 def vif_tolerance(df: pd.DataFrame, endog: str, drop_columns: list = []):
@@ -26,3 +28,41 @@ def vif_tolerance(df: pd.DataFrame, endog: str, drop_columns: list = []):
     df_metrics["tolerance"] = df_metrics["vif"] ** (-1)
 
     return df_metrics.sort_values("vif", ascending=False).reset_index(drop=True)
+
+
+def confusion_matrix(logit_model, cutoff: int, color: str = "Blues"):
+
+    classified = pd.Series(logit_model.predict() >= cutoff, name="Classified")
+
+    observed = pd.Series(logit_model.model.endog.astype(bool), name="Observed")
+
+    confusion_matrix = pd.crosstab(classified, observed)
+
+    plt.figure(figsize=(8, 6), dpi=80)
+
+    ax = sns.heatmap(
+        data=confusion_matrix,
+        cbar=False,
+        annot=True,
+        fmt="9.0f",
+        cmap=color,
+        annot_kws={"size": 18},
+    )
+
+    plt.xticks(fontsize=15)
+    plt.yticks(fontsize=15, rotation=0)
+
+    plt.xlabel(ax.get_xlabel(), fontsize=15)
+    plt.ylabel(ax.get_ylabel(), fontsize=15)
+
+    plt.suptitle("Confusion Matrix", fontsize=18)
+
+    return {
+        "Accuracy": accuracy_score(y_true=observed, y_pred=classified),
+        "Sensitivity": float(
+            recall_score(y_true=observed, y_pred=classified, pos_label=1)
+        ),
+        "Specificity": float(
+            recall_score(y_true=observed, y_pred=classified, pos_label=0)
+        ),
+    }
